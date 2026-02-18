8ef7aa0d54aaea0c83f293d2e7dc0dd74127941b
suichanwa <augustinnegura746@gmail.com>
2026-02-18 15:47:02 +0200

    upd

--- src/main/java/com/myname/mymodid/MyMod.java
+++ src/main/java/com/myname/mymodid/MyMod.java
@@ -1,23 +1,24 @@
 package com.myname.mymodid;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
+import cpw.mods.fml.common.FMLLog;
 
 @Mod(modid = MyMod.MODID, version = Tags.VERSION, name = "MyMod", acceptedMinecraftVersions = "[1.7.10]")
 public class MyMod {
 
     public static final String MODID = "mymodid";
-    public static final Logger LOG = LogManager.getLogger(MODID);
 
     @SidedProxy(clientSide = "com.myname.mymodid.ClientProxy", serverSide = "com.myname.mymodid.CommonProxy")
     public static CommonProxy proxy;
+
+    public static void logInfo(String message) {
+        FMLLog.info("[%s] %s", MODID, message);
+    }
 
     @Mod.EventHandler
     // preInit "Run before anything else. Read your config, create blocks, items, etc, and register them with the
